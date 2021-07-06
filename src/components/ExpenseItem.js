cf04408640795d52d97c17ba5832900cc8815c97
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-07-06 19:56:02 -0400

    Adds css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>
+  const expenseDate = new Date(2021, 6, 6);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 589.71
+
+  return (
+    <div className="expense-item">
+      <div>{ expenseDate.toISOString() }</div>
+      <div className="expense-item__description">
+        <h2>{ expenseTitle }</h2>
+        <div className="expense-item__price">${ expenseAmount }</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
