573c3001579f7d133892277e6177dba4c4e205aa
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-07-27 19:23:30 -0400

    Updates date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-us', {month: 'long'});
+  const day = props.date.toLocaleString('en-us', {year: '2-digit'});
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{ props.date.toISOString() }</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{ props.title }</h2>
         <div className="expense-item__price">${ props.amount }</div>
