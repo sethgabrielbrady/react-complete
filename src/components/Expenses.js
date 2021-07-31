2bc892bc943980bedcfe7047a8fd2e050580cbc9
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-07-31 17:58:36 -0400

    Adds card component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
+import Card from "./Card"
 
 function Expenses(props) {
   const expenses = props.expenses
   return (
-    <div className="expenses">
-    {expenses.map(function(expense, index){
+    <Card className="expenses">
+    {expenses.map(function(_expense, index){
       return (
         <ExpenseItem
           title={expenses[index].title}
@@ -14,7 +15,7 @@ function Expenses(props) {
         />
       );
      })}
-    </div>
+    </Card>
   )
 }
 
